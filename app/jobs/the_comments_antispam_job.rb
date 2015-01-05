a65198392d4adc2510427c2b84e7cbb636c6592a
Ilya N. Zykin <zykin-ilya@ya.ru>
2015-01-05 12:26:07 +0300

    fix comments

--- app/jobs/the_comments_antispam_job.rb
+++ app/jobs/the_comments_antispam_job.rb
@@ -1,8 +1,8 @@
 class TheCommentsAntispamJob < ActiveJob::Base
   queue_as :the_comments_jobs
 
-  # TheCommentsAntiSpamJob.perform_later(comment_id, { ... })
-  # TheCommentsAntiSpamJob.perform_now(comment_id, { ... })
+  # TheCommentsAntispamJob.perform_later(comment_id, { ... })
+  # TheCommentsAntispamJob.perform_now(comment_id, { ... })
   def perform(comment_id, request_data)
     comment = Comment.find(comment_id)
     comment.antispam_services_check_batch(request_data)
